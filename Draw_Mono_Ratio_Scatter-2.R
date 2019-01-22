a45f3c0d07f49bcaadaac4a5650a7af16f3c3473
Hazelhuangup <hazelhuang1993@icloud.com>
2019-01-22 16:37:46 +0000

    Update Draw_Mono_Ratio_Scatter-2.R

--- Draw_Mono_Ratio_Scatter-2.R
+++ Draw_Mono_Ratio_Scatter-2.R
@@ -1,16 +1,21 @@
 # Working directory
-setwd('~/Desktop/PhD Research/Drawing/')
+setwd('/Users/Mac/Documents/PhD Research/Drawing')
 
 # Libraries
-library('ggplot2')
+library(ggplot2)
 data <- read.csv('RAD_published_species.txt', NULL, header = TRUE)
 mydf <- data.frame(
-  Specie_Name= head(data$Material, n=39L), 
-  Monophyly_Ratio = as.numeric(head(data$Monophyletic_Ratio,n=39L)),
-  TaxonCategory = head(data$Kingdom,n=39L),
-  NoSpecies = as.numeric(head(data$No.N.1, n=39L))
+  Specie_Name= data$Material, 
+  Monophyly_Ratio = as.numeric(data$Monophyletic_Ratio),
+  TaxonCategory = data$Kingdom,
+  NoSpecies = as.numeric(data$No.N.1),
+  Seq_method = data$Seq_Method,
+  x = data$x
 )
-ggplot(mydf, aes(col = TaxonCategory, x = NoSpecies, y = Monophyly_Ratio)) + 
-  geom_point(size=3) +
+ggplot(mydf, aes(fill = TaxonCategory, col = TaxonCategory, x = NoSpecies, y = Monophyly_Ratio)) + 
+  geom_jitter(size=3) +
   theme_light()
-
+ggplot(mydf, aes(fill = Seq_method, col = Seq_method, x = x , y = Monophyly_Ratio), group = Seq_method) + 
+  geom_point(size=3) +
+  geom_line(size=1) +
+  theme_light()
