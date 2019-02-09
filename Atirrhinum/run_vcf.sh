374884038974d685df9550f13dff1a4bed52e6ea
Wu(Hazel) Huang <hazelhuang1993@icloud.com>
2019-02-09 13:09:01 +0000

    Update run_vcf.sh

--- Atirrhinum/run_vcf.sh
+++ Atirrhinum/run_vcf.sh
@@ -16,5 +16,19 @@ zcat ./01_raw_data/allsamples121217_var.SNPs_only.vcf.gz|awk '{split($10,a,":");
 ## re_correspond the species name to samples
 ./02_src/rename_samples.py ./01_raw_data/number2names.sh ./01_raw_data/antirrhinum_names.txt > ./01_raw_data/Sample_to_antirrhinum_names.txt
 
-## testing the species specific alleles in A.tortuosum (sample 7/8/9)
-./02_src/extract_species_specific_SNPs.py  ./01_raw_data/allsamples121217_SNPs_only.vcf.recode.vcf.gz ./01_raw_data/Sample_to_antirrhinum_names.txt ./01_raw_data/A.tortuosum.SSA.list
+## testing the species specific alleles in A.bolssierri (sample 7/8/9)
+./02_src/extract_species_specific_SNPs.py  ./01_raw_data/allsamples121217_SNPs_only.vcf.recode.vcf.gz ./01_raw_data/Sample_to_antirrhinum_names.txt ./01_raw_data/A.bolssierri.SSA
+
+## extract species specific alleles in all species
+./02_src/extract_species_specific_SNPs.py  ./01_raw_data/allsamples121217_SNPs_only.vcf.recode.vcf.gz ./01_raw_data/Sample_to_antirrhinum_names.txt 
+
+## basic statistical analysis and visualization of SSA
+for i in `ls ./03_output/A.*list`;do
+            wc -l $i >> ./03_output/Atirrhinum.SSA_number_for_every_species.list
+            if [ -s $i ]; then
+                        ./02_src/sliding_window.py $i $i\.sliding_win.freq
+            fi
+done
+sort -n ./03_output/Atirrhinum.SSA_number_for_every_species.list
+
+Rscript ./02_src/SSA_frequency_sliding_windows_on_chr.R ./03_output/
