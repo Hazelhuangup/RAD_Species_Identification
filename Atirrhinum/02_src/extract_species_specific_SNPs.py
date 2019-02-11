9ff2d78c85119bfe0c3ec7b919eea7b4682b9ee7
Wu(Hazel) Huang <hazelhuang1993@icloud.com>
2019-02-11 16:51:01 +0000

    Add files via upload

--- Atirrhinum/02_src/extract_species_specific_SNPs.py
+++ Atirrhinum/02_src/extract_species_specific_SNPs.py
@@ -5,17 +5,19 @@ import gzip
 
 vcf_file = gzip.open(sys.argv[1])
 name_file = open(sys.argv[2])
-out_file = open(sys.argv[3],'w')
-order_sample = open('../01_raw_data/vcf_file_order.list')
+order_sample = open('./01_raw_data/vcf_file_order.list')
 
 name_dict = {}
+name_list = []
 
 for i in name_file:
 	i = i.strip().split()
 	if i[2] not in name_dict:
 		name_dict[i[2]] = [int(i[0].split('_')[1])]
+		name_list.append(int(i[0].split('_')[1]))
 	else:
 		name_dict[i[2]].append(int(i[0].split('_')[1]))
+		name_list.append(int(i[0].split('_')[1]))
 
 position_in_vcf = []
 for i in order_sample:
@@ -27,11 +29,7 @@ for i in order_sample:
 		else:
 			position_in_vcf.append('')
 
-#print position_in_vcf
-
-#for i in name_dict:
-#	print i,name_dict[i]
-
+# for every SNP locus, put all individuals's allele in a list: [0,1,1,0,01,12,1,12,2,012....]
 for i in vcf_file:
 	if not i.startswith('#'):
 		all_alleles = ['']*120
@@ -42,27 +40,28 @@ for i in vcf_file:
 			for alt in range(0,len(alleles)):
 				if int(alleles[alt]) > 0:
 					all_alleles[order] = all_alleles[order]+str(alt)
-#		print all_alleles
-
-		rest_alleles = []
-		species_alleles = []
+# for every species, put its individuals into a list, the rest another list.
 		for A in name_dict:
-			if A == 'A.boissieri':
-				for individuals in name_dict[A]:
-					species_alleles.append(all_alleles[position_in_vcf.index(individuals)])
-#				print 'the allele status of ',A,' is: ',species_alleles  ## the allele status of  A.boissieri  is:  ['1', '1', '1']
-			else:
-				for individuals in name_dict[A]:
-					rest_alleles.append(all_alleles[position_in_vcf.index(individuals)])
-#		print  'the allele status of the rest is: ', rest_alleles
+			rest_alleles = []
+			species_alleles = []
+			rest_individual_list = name_list[:]
+			out_file = open('./03_output/'+A+'.SSA.list','a')
+			for individuals in name_dict[A]:
+				rest_individual_list.remove(individuals)
+				species_alleles.append(all_alleles[position_in_vcf.index(individuals)])
+			for individuals in rest_individual_list:
+				rest_alleles.append(all_alleles[position_in_vcf.index(individuals)])
+# judge if all individuals in species A are consensus and different from all the rest species.
+			if len(set(species_alleles)) == 1 and set(species_alleles) != set(rest_alleles):
+				if set(rest_alleles) > set(species_alleles):  ## alleles shows not only in A
+					continue
+				else:
+					out_file.write('{0}\t{1}\t{2}\tSSL\n'.format(i[0],i[1],A))
 
-		if len(set(species_alleles)) == 1 and set(species_alleles) != set(species_alleles):
-			if set(rest_alleles) > set(species_alleles):  ## alleles shows not only in A
-				continue
-			else:
-				print 'the allele status of ',A,' is: ',species_alleles  ## the allele status of  A.boissieri  is:  ['1', '1', '1']
-				print  'the allele status of the rest is: ', rest_alleles
-				out_file.write('{0}\t{1}\t{2}\tSSL\n'.format(i[0],i[1],A))
+for A in name_dict:
+	file_name = A+'.SSA.list'
+	file_name.close()
+
 '''
 		else:
 			print frequency
@@ -70,4 +69,3 @@ for i in vcf_file:
 
 vcf_file.close()
 name_file.close()
-out_file.close()
